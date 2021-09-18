1521717273802933503b97b394b69e07acba5847
borisoft82 <borisoft82@gmail.com>
2021-09-18 21:35:05 +0200

    vue setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 require('./bootstrap');
+
+import Vue from 'vue';
+import PostsIndex from './components/posts/Index'
+
+//Vue.component('posts-index', require('./components/posts/Index.vue').default);
+
+const app = new Vue({
+    el: '#app',
+    components: {
+        PostsIndex
+    }
+});
