2e11a90523239c2f142027223e75b4a319991d24
borisoft82 <borisoft82@gmail.com>
2022-02-08 21:29:42 +0100

    display validation errors

--- resources/js/components/posts/Create.vue
+++ resources/js/components/posts/Create.vue
@@ -5,10 +5,16 @@
         <div class="form-group">
           <label>Title</label>
           <input type="text" v-model="fields.title" class="form-control">
+          <div class="alert alert-danger" v-if="errors && errors.title">
+            {{ errors.title[0] }}
+          </div>
         </div>
         <div class="form-group">
           <label>Text</label>
           <textarea v-model="fields.text" class="form-control" rows="3"></textarea>
+          <div class="alert alert-danger" v-if="errors && errors.text">
+            {{ errors.text[0] }}
+          </div>
         </div>
         <div class="form-group">
           <label>Category</label>
@@ -19,6 +25,9 @@
               {{ category.name }}
               </option>
           </select>
+          <div class="alert alert-danger" v-if="errors && errors.category_id">
+            {{ errors.category_id[0] }}
+          </div>
         </div>
         <button type="submit" class="btn btn-primary">Save</button>
       </form>
@@ -35,7 +44,8 @@
           title: '',
           text: '',
           category_id: ''
-        }
+        },
+        errors: {}
       }
     },
 
@@ -51,6 +61,10 @@
         axios.post('api/posts', this.fields)
           .then(response => {
             this.$router.push('/');
+        }).catch(error => {
+          if(error.response.status === 422) {
+            this.errors = error.response.data.errors;
+          }
         })
       }
     }
